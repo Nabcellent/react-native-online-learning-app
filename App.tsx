c92d045943d8c268b38705b9e6e6661e26df75f5
Nabcellent <nabcellent.dev@gmail.com>
2022-08-26 21:23:36 +0300

    Adds splashscreen and fixes fonts
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { MainLayout } from './screens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
 
 const Stack = createNativeStackNavigator();
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+    let [fontsLoaded] = useFonts({
+        'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    });
+
+    useEffect(() => {
+        async function prepare() {
+            if (fontsLoaded) await SplashScreen.hideAsync();
+        }
+
+        prepare();
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={'Dashboard'}>
@@ -14,12 +33,3 @@ export default function App() {
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
