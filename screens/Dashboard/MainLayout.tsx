3b6543aed6864e64269967fc4e56c75259cd162b
Nabcellent <nabcellent.dev@gmail.com>
2022-08-27 10:48:11 +0300

    Adds main layout
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- screens/Dashboard/MainLayout.tsx
+++ screens/Dashboard/MainLayout.tsx
@@ -1,5 +1,5 @@
-import React, { createRef, useEffect, useRef, useState } from 'react';
-import { Animated, Image, Text, TouchableOpacity, View } from 'react-native';
+import React, { createRef, useCallback, useEffect, useRef, useState } from 'react';
+import { Animated, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { COLORS, FONTS, SIZES } from '../../constants';
 import constants from '../../constants/constants';
 import Home from './Home';
@@ -29,8 +29,8 @@ const TabIndicator = ({ measureLayout, scrollX }: { measureLayout: MeasureLayout
     );
 };
 
-const Tabs = ({ scrollX }: { scrollX: Animated.Value }) => {
-    const containerRef = useRef();
+const Tabs = ({ scrollX, onTabPress }: { onTabPress: (tabIndex: number) => void, scrollX: Animated.Value }) => {
+    const containerRef = useRef(null);
     const [measureLayout, setMeasureLayout] = useState<MeasureLayout[]>([]);
 
     useEffect(() => {
@@ -47,16 +47,14 @@ const Tabs = ({ scrollX }: { scrollX: Animated.Value }) => {
 
     return (
         <View ref={containerRef} style={{ flex: 1, flexDirection: 'row' }}>
-            {measureLayout.length > 0 && (
-                <TabIndicator measureLayout={measureLayout} scrollX={scrollX}/>
-            )}
+            {measureLayout.length > 0 && <TabIndicator measureLayout={measureLayout} scrollX={scrollX}/>}
             {bottomTabs.map((item, i) => (
                 <TouchableOpacity key={`tab-${i}`} ref={item.ref} style={{
                     flex: 1,
                     paddingHorizontal: 15,
                     alignItems: 'center',
                     justifyContent: 'center'
-                }}>
+                }} onPress={() => onTabPress(i)}>
                     <Image source={item.icon} resizeMode={'contain'} style={{ width: 25, height: 25 }}/>
                     <Text style={{ marginTop: 3, color: COLORS.white, ...FONTS.h3 }}>{item.label}</Text>
                 </TouchableOpacity>
@@ -66,13 +64,18 @@ const Tabs = ({ scrollX }: { scrollX: Animated.Value }) => {
 };
 
 const MainLayout = () => {
-    const flatListRef = useRef();
-    const scrollX = useRef(new Animated.Value(0)).current;
+    const flatListRef = useRef<FlatList<{ id: number; label: string; icon: any; }>>(null);
+    const scrollX = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+    const onTabPress = useCallback((tabIndex: number) => {
+        flatListRef?.current?.scrollToOffset({ offset: tabIndex * SIZES.width });
+    }, []);
 
     return (
         <View style={{ flex: 1, backgroundColor: COLORS.white }}>
             <View style={{ flex: 1 }}>
                 <Animated.FlatList ref={flatListRef} horizontal pagingEnabled snapToAlignment={'center'}
+                                   scrollEnabled={false}
                                    snapToInterval={SIZES.width} decelerationRate={'fast'}
                                    showsHorizontalScrollIndicator={false} data={constants.bottom_tabs}
                                    keyExtractor={item => `main-${item.id}`}
@@ -89,7 +92,7 @@ const MainLayout = () => {
             <View style={{ marginBottom: 20, paddingHorizontal: SIZES.padding, paddingVertical: SIZES.radius }}>
                 <Shadow style={{ width: SIZES.width - (SIZES.padding * 2), height: 85 }}>
                     <View style={{ flex: 1, borderRadius: SIZES.radius, backgroundColor: COLORS.primary3 }}>
-                        <Tabs scrollX={scrollX}/>
+                        <Tabs scrollX={scrollX} onTabPress={onTabPress}/>
                     </View>
                 </Shadow>
             </View>
